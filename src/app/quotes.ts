d5a6139af54ca7fbb3396769c268beac7db1f886
FGacheru <francisgacheru2001@gmail.com>
2020-11-09 10:17:02 +0300

    add a constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,18 +1,6 @@
-// export class Quotes {
-//   like : number;
-//   unlike : number;
-//     showDescription: boolean;
-//     constructor(public id: number,public name: string,public description: string, public completeDate: Date){
-//       this.showDescription=false;
-//       this.like = 0;
-//       this.unlike = 0;
-//     }
-//   }
-  export class Quotes {
+ export class Quotes {
     id: number;
-    name: string;
-    // author: string;
-    // submitter: string;
+    name: string;               
     description : string;
     completeDate: Date;
     like: number;
@@ -21,12 +9,10 @@
     constructor( id: number,name: string,description: string,completeDate: Date){
         this.id = id;
         this.name = name;
-        // this.author = author;
         this.completeDate = completeDate;
         this.like = 0;
         this.unlike = 0;
         this.description = description;
-        // this.submitter = submitter;
         this.showDescription = false;
     }
 }
