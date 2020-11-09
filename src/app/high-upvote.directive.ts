97074f2a473f31d979d72df4d4f9635a6e6b0a5b
FGacheru <francisgacheru2001@gmail.com>
2020-11-09 10:21:41 +0300

    add color to highest likes

---
+++ src/app/high-upvote.directive.ts
@@ -0,0 +1,24 @@
+// import { Directive } from '@angular/core';
+
+// @Directive({
+//   selector: '[appHighUpvote]'
+// })
+// export class HighUpvoteDirective {
+
+//   constructor() { }
+
+// }
+import { Directive,ElementRef,Input } from '@angular/core';
+@Directive({
+  selector: '[appHighUpvote]'
+})
+export class HighUpvoteDirective {
+constructor(private el: ElementRef) {
+  this.highlight(this.highlightColor || this.defaultColor || 'red');
+ }
+@Input() defaultColor: string;
+@Input('myHighlight') highlightColor: string;
+private highlight(color: string) {
+  this.el.nativeElement.style.backgroundColor = color;
+}
+}
